ec3ccfa3fa86c54e26060b330918d751af727bea
DmitryLorents <zmey.task@gmail.com>
2023-05-11 21:37:25 +0600

    Create model and network managers

---
+++ GeographicAtlas/Model/Model.swift
@@ -0,0 +1,11 @@
+//
+//  DataStorage.swift
+//  GeographicAtlas
+//
+//  Created by Дмитрий Лоренц on 11.05.2023.
+//
+
+import Foundation
+struct Country {
+    
+}
---
+++ GeographicAtlas/Network/DownloadManager.swift
@@ -0,0 +1,12 @@
+//
+//  DownloadManager.swift
+//  GeographicAtlas
+//
+//  Created by Дмитрий Лоренц on 11.05.2023.
+//
+
+import Foundation
+
+class  DownloadManager {
+    let urlString = ""
+}
