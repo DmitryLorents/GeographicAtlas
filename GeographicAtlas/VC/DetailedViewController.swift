c2e72b521bf12f5aeda89f5e2eb2286e19ac36e8
DmitryLorents <zmey.task@gmail.com>
2023-05-12 09:33:35 +0600

    DetailedViewController add init with country

--- GeographicAtlas/VC/DetailedViewController.swift
+++ GeographicAtlas/VC/DetailedViewController.swift
@@ -7,16 +7,29 @@
 
 import UIKit
 
-class DetailedViewController: UIViewController {
+final class DetailedViewController: UIViewController {
 
     //MARK:  - Constants, variables & outlets
+    var  country: Country?
     var tableViewDetailed: UITableView = {
         let table = UITableView()
         table.translatesAutoresizingMaskIntoConstraints = false
         table.backgroundColor = .yellow
         return table
     }()
-
+    
+    //MARK: - Init
+    
+    init(country: Country) {
+        super.init(nibName: nil, bundle: nil)
+        self.title = "Country name"
+        self.country = country
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 //MARK: - Load view
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +40,8 @@ class DetailedViewController: UIViewController {
     }
     //MARK: - Functions
     private func setOutlets()  {
+        view.backgroundColor = .systemBackground
+        
         tableViewDetailed.delegate = self
         tableViewDetailed.dataSource = self
     }
